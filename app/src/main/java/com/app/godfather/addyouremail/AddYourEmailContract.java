659d91c8a8c868f670cac9366f1cf13893982e0c
Gaberu <gabrielrosadn@gmail.com>
2016-10-22 00:41:18 -0300

    add your experience initial structure

--- app/src/main/java/com/app/godfather/addyouremail/AddYourEmailContract.java
+++ app/src/main/java/com/app/godfather/addyouremail/AddYourEmailContract.java
@@ -4,4 +4,15 @@ package com.app.godfather.addyouremail;
  * Created by root on 21/10/16.
  */
 public interface AddYourEmailContract {
+
+    interface View {
+
+        void goToNextScreen();
+
+    }
+
+    interface Presenter{
+
+    }
+
 }

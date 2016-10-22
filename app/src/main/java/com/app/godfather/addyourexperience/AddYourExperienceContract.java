fa44f6f6e0427a046374128d1ca624af3c644c9d
Gaberu <gabrielrosadn@gmail.com>
2016-10-22 01:10:39 -0300

    final screen flow

---
+++ app/src/main/java/com/app/godfather/addyourexperience/AddYourExperienceContract.java
@@ -0,0 +1,18 @@
+package com.app.godfather.addyourexperience;
+
+/**
+ * Created by root on 22/10/16.
+ */
+public interface AddYourExperienceContract {
+
+    interface View {
+
+        void sendYourExperience();
+
+    }
+
+    interface Presenter{
+
+    }
+
+}
--- app/src/main/java/com/app/godfather/addyourexperience/AddYourExperienceFragment.java
+++ app/src/main/java/com/app/godfather/addyourexperience/AddYourExperienceFragment.java
@@ -1,20 +1,23 @@
 package com.app.godfather.addyourexperience;
 
 import android.os.Bundle;
-import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.app.godfather.R;
+import com.app.godfather.addyouremail.AddYourEmailContract;
 
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 /**
  * Created by root on 22/10/16.
  */
-public class AddYourExperienceFragment extends Fragment {
+public class AddYourExperienceFragment extends Fragment implements AddYourEmailContract.View{
+
 
     public static AddYourExperienceFragment newInstance(){
         AddYourExperienceFragment fragment = new AddYourExperienceFragment();
@@ -22,13 +25,18 @@ public class AddYourExperienceFragment extends Fragment {
         return fragment;
     }
 
-    @Nullable
     @Override
-    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.add_your_experience_frag, container, false);
         ButterKnife.bind(this, view);
 
 
         return view;
+    }
+
+    @Override
+    @OnClick(R.id.enviar_experiencia)
+    public void goToNextScreen() {
+        Toast.makeText(getContext(), "ENVIADO", Toast.LENGTH_SHORT).show();
     }
 }
